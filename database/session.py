51d60700672426f161fa5efde749251e78330f72
taliban-fr <kmikekioko1@gmail.com>
2026-08-08 03:08:36 +0300

    Prepare for Render deployment

--- database/session.py
+++ database/session.py
@@ -5,7 +5,7 @@ import os
 load_dotenv()
 
 DATABASE_URL = os.getenv("DATABASE_URL")
-engine = create_engine(DATABASE_URL, echo=True)
+engine = create_engine(DATABASE_URL, echo=os.getenv("DEBUG", "false").lower() == "true")
 
 def create_db_and_tables():
     from sqlmodel import SQLModel
